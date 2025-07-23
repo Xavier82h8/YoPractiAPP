c440ea18270cd68af22b15290e92b2c7ddf72359
Javi Andraft <javiandraft@gmail.com>
2025-07-23 03:59:27 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,15 +1,16 @@
+'use client';
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration - DO NOT EDIT
 const firebaseConfig = {
+  "projectId": "yopracticando-36seb",
+  "appId": "1:634933837473:web:0b76c4502219798642bae0",
+  "storageBucket": "yopracticando-36seb.appspot.com",
   "apiKey": "AIzaSyAGPUVrINy_j1g-_Um1xZ21GODtfe9NtkY",
   "authDomain": "yopracticando-36seb.firebaseapp.com",
-  "projectId": "yopracticando-36seb",
-  "storageBucket": "yopracticando-36seb.appspot.com",
-  "messagingSenderId": "634933837473",
-  "appId": "1:634933837473:web:0b76c4502219798642bae0"
+  "messagingSenderId": "634933837473"
 };
 
 // Initialize Firebase for SSR safely
